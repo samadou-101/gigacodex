3e520376c486027fa9a630ee29abfd1b01ebc08d
samadou <mossabm036@gmail.com>
2025-08-31 13:28:18 +0100

    ai config

---
+++ backend/src/config/ai.config.ts
@@ -0,0 +1,6 @@
+// aiClient.ts
+import { GoogleGenAI } from "@google/genai";
+
+export const ai = new GoogleGenAI({
+  apiKey: process.env.GOOGLE_API_KEY, // Use env variable
+});
--- backend/src/modules/ai/generation/workers/plan.worker.ts
+++ backend/src/modules/ai/generation/workers/plan.worker.ts
@@ -1,10 +1,16 @@
 import { redis } from "@/config/redis.config.js";
 import { Worker } from "bullmq";
+import { ai } from "@/config/ai.config.js";
 
 const planWorker = new Worker(
   "planQueue",
   async (job) => {
-    console.log(job.data);
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: "hello",
+    });
+    // console.log(job.data);
+    console.log(response.text);
   },
   { connection: redis }
 );
